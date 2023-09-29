b65b383e2daf5ee76e794a64ba89b0fbcc999e1d
Edgar Robitaille <edgarrobitaille@MacBook-Air-4.local>
2023-09-28 23:59:49 -0400

    Integrated DFRobot pH probe

--- Marlin/src/module/temperature.cpp
+++ Marlin/src/module/temperature.cpp
@@ -2123,6 +2123,56 @@ void Temperature::task() {
   int pHArray[ArrayLenth];
   int pHArrayIndex = 0;
 
+  double avergearray(int* arr, int number){
+    int i;
+    int max,min;
+    double avg;
+    long amount = 0;
+    if(number <= 0){
+      Serial.println("Error number for the array to averaging!/n");
+      return 0;
+    }
+    if(number < 5){   //less than 5, calculated directly statistics
+      for(i=0;i<number;i++){
+        amount+=arr[i];
+      }
+      avg = amount/number;
+      return avg;
+    }else{
+      if(arr[0]<arr[1]){
+        min = arr[0];max=arr[1];
+      }
+      else{
+        min=arr[1];max=arr[0];
+      }
+      for(i=2;i<number;i++){
+        if(arr[i]<min){
+          amount+=min;        //arr<min
+          min=arr[i];
+        }else {
+          if(arr[i]>max){
+            amount+=max;    //arr>max
+            max=arr[i];
+          }else{
+            amount+=arr[i]; //min<=arr<=max
+          }
+        }
+      }
+      avg = (double)amount/(number-2);
+    }
+    return avg;
+  }
+
+  static float pHValue, voltage;
+  void update_pH() {
+      static unsigned long samplingTime = millis();
+      pHArray[pHArrayIndex++] = analogRead(SensorPin);
+      if (pHArrayIndex == ArrayLenth) pHArrayIndex = 0;
+      voltage = avergearray(pHArray, ArrayLenth) * 5.0 / 1024;
+      pHValue = -5.99 * voltage + Offset;
+      samplingTime = millis();
+  }
+
   celsius_float_t Temperature::user_thermistor_to_deg_c(const uint8_t t_index, const raw_adc_t raw) {
 
     if (!WITHIN(t_index, 0, COUNT(user_thermistor) - 1)) return 25;
